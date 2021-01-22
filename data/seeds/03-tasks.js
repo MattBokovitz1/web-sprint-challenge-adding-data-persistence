8a1b8646b7f748ea88a5e1d8cb606e362b4d7e67
Matthew Bokovitz <MattBokovitz@Matthews-MBP-2.fios-router.home>
2021-01-22 13:37:42 -0500

    continuing to fill out seed data

--- data/seeds/03-tasks.js
+++ data/seeds/03-tasks.js
@@ -3,9 +3,13 @@ exports.seed = function (knex) {
   return knex("tasks")
     .truncate()
     .then(function () {
-      // Inserts seed entries
       return knex("tasks").insert([
-        { task_completed: false, task_description: "Do foo", task_notes: "" },
+        {
+          task_description: "Do foo",
+          task_notes: "",
+          task_completed: false,
+          project_id: 1,
+        },
       ]);
     });
 };
